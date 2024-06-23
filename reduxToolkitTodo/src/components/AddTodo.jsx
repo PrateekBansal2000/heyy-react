9e680abccf3d8fb4a9b11bfce318572d6991b229
Prateek <prateekb133@gmail.com>
2024-06-23 16:02:13 +0530

    Created a Todo project using the redux react toolkit.

---
+++ reduxToolkitTodo/src/components/AddTodo.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { addTodo } from "../features/todo/todoSlice";
+
+function AddTodo() {
+  const [input, setInput] = React.useState("");
+  const dispatch = useDispatch();
+
+  const addTodoHandler = (e) => {
+    e.preventDefault();
+    dispatch(addTodo(input));
+    setInput("");
+  };
+  return (
+    <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
+      <input
+        type="text"
+        className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+        placeholder="Enter a Todo..."
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button
+        class="relative px-8 py-2 rounded-md bg-white isolation-auto z-10 border-2 border-lime-500
+        before:absolute before:w-full before:transition-all before:duration-100 before:hover:w-full before:-right-full before:hover:right-0 before:rounded-full  before:bg-lime-500 before:-z-10  before:aspect-square before:hover:scale-150 overflow-hidden before:hover:duration-500"
+      >
+        Add Item
+      </button>
+    </form>
+  );
+}
+
+export default AddTodo;
